ed804fde65ca51a0b54a0d01dda38f0e69455eb4
logicfirst <slee@logicfirstllc.com>
2021-01-11 20:29:50 -0500

    commit tree 56f43a0bdc80dde7df9b4246428619a4fcedf138

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+  drivers.push(name)
+};
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name)
+};
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop()
+}; 
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift()
+};
+
+function appendDriver(name){
+  let copyOfDriver = [...drivers, name]
+};
+
+function prependDriver(name){
+  let copyOfDriver = [name, ...drivers]
+};
+
+function removeLastDriver(){
+  drivers.slice(0, drivers.length - 1)
+};
+
+function removeFirstDriver(){
+  drivers.slice(1)
+};
+
